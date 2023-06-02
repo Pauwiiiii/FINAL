ba8fc9437c84efc448fc03783a7634fef69b4665
Pawiiiii <johnpaulsolis35@gmail.com>
2023-06-02 10:15:43 +0800

    Connecting data base and test the table named employees

---
+++ api.py
@@ -0,0 +1,37 @@
+from flask import Flask, make_response, jsonify
+from flask_mysqldb import MySQL
+
+
+app = Flask(__name__)
+app.config["MYSQL_HOST"] = "127.0.0.1"
+app.config["MYSQL_USER"] = "root"
+app.config["MYSQL_PASSWORD"] = "root"
+app.config["MYSQL_DB"] = "employees"
+
+app.config["MYSQL_CURSORCLASS"] = "DictCursor"
+
+#Coonecting to mysql app
+mysql = MySQL(app)
+@app.route("/")
+
+
+@app.route("/employees", methods=["GET"])
+def get_employees():
+    cur =mysql.connection.cursor()
+    query = """
+    select * from employees
+    """
+    cur.execute(query)
+    data = cur.fetchall()
+    cur.close()
+
+    return make_response(jsonify(data), 200)
+
+
+
+def hello_world():
+    return "<p>Hello, World!</p>"
+
+if __name__ == "__main__":
+    app.run(debug=True)
+
