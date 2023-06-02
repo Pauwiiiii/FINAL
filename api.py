56b23573698ee1d449003ead7e74b9bace2196fa
Pawiiiii <johnpaulsolis35@gmail.com>
2023-06-02 22:17:38 +0800

    Refract the code

--- api.py
+++ api.py
@@ -13,37 +13,46 @@ app.config["MYSQL_CURSORCLASS"] = "DictCursor"
 #Coonecting to mysql app
 mysql = MySQL(app)
 @app.route("/")
+def hello_world():
+    return "<p>Hello, World!</p>"
+
+
+# Refractor of code
+def data_fetch(query):
+    cur = mysql.connection.cursor()
+    cur.execute(query)
+    data = cur.fetchall()
+    cur.close()
+    return data
+
 
 # Display query from employees data base and get from web application
 @app.route("/dept_emp", methods=["GET"])
 def get_dept_emp():
-    cur =mysql.connection.cursor()
-    query = """
-    select * from dept_emp w
-    """
-    cur.execute(query)
-    data = cur.fetchall()
-    cur.close()
-
+    # cur = mysql.connection.cursor()
+    data = data_fetch("""SELECT * FROM dept_emp""")
     return make_response(jsonify(data), 200)
+    # query = """
+    #select * from dept_emp w
+    #"""
+    # '''cur.execute(query)
+    # data = cur.fetchall()
+    # cur.close() ''' 
+ 
+    
 
 # New function before refractor
 @app.route("/dept_emp/<int:emp_no>", methods=["GET"])
 def get_dept_emp_by_emp_no(emp_no):
-    cur =mysql.connection.cursor()
-    query = """
-    select * from dept_emp where emp_no = {}
-    """.format(emp_no)
-    cur.execute(query)
-    data = cur.fetchall()
-    cur.close()
-
+    # cur =mysql.connection.cursor()
+    data = data_fetch("""select * from dept_emp where emp_no = {}""".format(emp_no))
     return make_response(jsonify(data), 200)
-
-
-
-def hello_world():
-    return "<p>Hello, World!</p>"
+    # query = """
+    # select * from dept_emp where emp_no = {}
+    # """.format(emp_no)
+    # cur.execute(query)
+    # data = cur.fetchall()
+    # cur.close()
 
 if __name__ == "__main__":
     app.run(debug=True)
