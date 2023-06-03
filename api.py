f79a9ddf43b4eed1fbb1f7027194e624307940c6
Pawiiiii <johnpaulsolis35@gmail.com>
2023-06-03 11:08:21 +0800

    Fixing the code and adding error handleng & input validation

--- api.py
+++ api.py
@@ -17,39 +17,54 @@ mysql = MySQL(app)
 def hello_world():
     return "<p>SOLIS, JOHN PAUL (FINALS)!</p>"
 
-# Refracted code
-def data_fetch(query):
+
+# Utility function to fetch data from MySQL
+def data_fetch(query, params=None):
     cur = mysql.connection.cursor()
-    cur.execute(query)
+    cur.execute(query, params)
     data = cur.fetchall()
     cur.close()
     return data
 
-# Get Method for dept_table
+
+# Get Method for dept_emp table
 @app.route("/dept_emp", methods=["GET"])
 def get_dept_emp():
-    data = data_fetch("SELECT * FROM dept_emp")
-    return make_response(jsonify(data), 200)
-
-# Get method for dept_method using the emp_no
-@app.route("/dept_emp/<int:emp_no>", methods=["GET"])
-def get_dept_emp_by_emp_no(emp_no):
-    query = "SELECT * FROM dept_emp WHERE emp_no = {}".format(emp_no)
+    query = "SELECT * FROM dept_emp"
     data = data_fetch(query)
     return make_response(jsonify(data), 200)
+
+
+# Get method for dept_emp using the emp_no
+@app.route("/dept_emp/<int:emp_no>", methods=["GET"])
+def get_dept_emp_by_emp_no(emp_no):
+    query = "SELECT * FROM dept_emp WHERE emp_no = %s"
+    data = data_fetch(query, (emp_no,))
+    
+    if not data:
+        return make_response(jsonify({"error": "No department employee found with the given emp_no"}), 404)
+    
+    return make_response(jsonify(data), 200)
+
 
 # Post Method 
 @app.route("/dept_emp", methods=["POST"])
 def add_dept_emp():
     info = request.get_json()
 
+    if not all(key in info for key in ["emp_no", "dept_no", "from_date", "to_date"]):
+        return make_response(jsonify({"error": "Missing required fields"}), 400)
+    
     emp_no = info["emp_no"]
     dept_no = info["dept_no"]
     from_date_str = info["from_date"]
     to_date_str = info["to_date"]
 
-    from_date = datetime.strptime(from_date_str, "%a, %d %b %Y %H:%M:%S %Z").date()
-    to_date = datetime.strptime(to_date_str, "%a, %d %b %Y %H:%M:%S %Z").date()
+    try:
+        from_date = datetime.strptime(from_date_str, "%a, %d %b %Y %H:%M:%S %Z").date()
+        to_date = datetime.strptime(to_date_str, "%a, %d %b %Y %H:%M:%S %Z").date()
+    except ValueError:
+        return make_response(jsonify({"error": "Invalid date format"}), 400)
 
     with mysql.connection.cursor() as cur:
         cur.execute("INSERT INTO dept_emp (emp_no, dept_no, from_date, to_date) VALUES (%s, %s, %s, %s)",
@@ -57,7 +72,7 @@ def add_dept_emp():
         mysql.connection.commit()
         rows_affected = cur.rowcount
 
-    return make_response(jsonify({"Message": "Department employee added successfully",
+    return make_response(jsonify({"message": "Department employee added successfully",
                                    "rows_affected": rows_affected}), 201)
 
 
@@ -66,31 +81,44 @@ def add_dept_emp():
 def update_dept_emp(emp_no):
     info = request.get_json()
 
+    if not all(key in info for key in ["dept_no", "from_date", "to_date"]):
+        return make_response(jsonify({"error": "Missing required fields"}), 400)
+
     dept_no = info["dept_no"]
     from_date_str = info["from_date"]
     to_date_str = info["to_date"]
 
-    from_date = datetime.strptime(from_date_str, "%a, %d %b %Y %H:%M:%S %Z").date()
-    to_date = datetime.strptime(to_date_str, "%a, %d %b %Y %H:%M:%S %Z").date()
+    try:
+        from_date = datetime.strptime(from_date_str, "%a, %d %b %Y %H:%M:%S %Z").date()
+        to_date = datetime.strptime(to_date_str, "%a, %d %b %Y %H:%M:%S %Z").date()
+    except ValueError:
+        return make_response(jsonify({"error": "Invalid date format"}), 400)
+
+    query = "UPDATE dept_emp SET dept_no = %s, from_date = %s, to_date = %s WHERE emp_no = %s"
+    params = (dept_no, from_date, to_date, emp_no)
 
     with mysql.connection.cursor() as cur:
-        cur.execute("UPDATE dept_emp SET dept_no = %s, from_date = %s, to_date = %s WHERE emp_no = %s",
-                    (dept_no, from_date, to_date, emp_no))
+        cur.execute(query, params)
         mysql.connection.commit()
         rows_affected = cur.rowcount
 
-    return make_response(jsonify({"Message": "Department employee updated successfully",
+    return make_response(jsonify({"message": "Department employee updated successfully",
                                    "rows_affected": rows_affected}), 200)
+
 
 # Delete method for the dept_emp table
 @app.route("/dept_emp/<int:emp_no>", methods=["DELETE"])
 def del_dept_emp(emp_no):
-    cur = mysql.connection.cursor()
-    cur.execute("""DELETE FROM dept_emp WHERE emp_no = %s""", (emp_no,))
-    mysql.connection.commit()
-    rows_affected = cur.rowcount
-    cur.close()
-    return make_response(jsonify({"Message": "Dept_emp deleted successfully", "rows_affected": rows_affected}), 200)
+    with mysql.connection.cursor() as cur:
+        cur.execute("DELETE FROM dept_emp WHERE emp_no = %s", (emp_no,))
+        mysql.connection.commit()
+        rows_affected = cur.rowcount
+
+    if rows_affected == 0:
+        return make_response(jsonify({"error": "No department employee found with the given emp_no"}), 404)
+
+    return make_response(jsonify({"message": "Dept_emp deleted successfully",
+                                   "rows_affected": rows_affected}), 200)
 
 
 if __name__ == "__main__":
