20411db49fbfc9ac4dff47cbd3257ca06697589c
Pawiiiii <johnpaulsolis35@gmail.com>
2023-06-03 15:17:06 +0800

    Modifying the code

--- api.py
+++ api.py
@@ -1,14 +1,13 @@
-# Flask API for interacting with a MySQL database table #dept_emp.
-# Includes various endpoints for retrieving, adding, updating, and deleting records.
-# Supports both JSON and XML response formats.
-
-from flask import Flask, make_response, jsonify, request, Response
+# Imporing process
+from flask import Flask, jsonify, request, Response
 from flask_mysqldb import MySQL
 from datetime import datetime
 import xml.etree.ElementTree as ET
 
 app = Flask(__name__)
-app.config["MYSQL_HOST"] = "127.0.0.1"
+
+# MySQL configurations
+app.config["MYSQL_HOST"] = "localhost"
 app.config["MYSQL_USER"] = "root"
 app.config["MYSQL_PASSWORD"] = "root"
 app.config["MYSQL_DB"] = "employees"
@@ -17,117 +16,134 @@ app.config["MYSQL_CURSORCLASS"] = "DictCursor"
 # Connecting to MySQL database
 mysql = MySQL(app)
 
-# Root route
-@app.route("/")
-def hello_world():
-    return "<p>SOLIS, JOHN PAUL (FINALS)!</p>"
 
-# Utility function to fetch data from MySQL
-def data_fetch(query, params=None):
+def generate_xml_response(data):
+    # Generate XML response based on data provided
+    root = ET.Element("dept_emp")
+
+    if isinstance(data, dict):
+        employee = ET.SubElement(root, "employee")
+        for key, value in data.items():
+            ET.SubElement(employee, key).text = str(value)
+    elif isinstance(data, list):
+        for item in data:
+            employee = ET.SubElement(root, "employee")
+            for key, value in item.items():
+                ET.SubElement(employee, key).text = str(value)
+    else:
+        # Return error response if data format is invalid
+        return Response(response=jsonify({"error": "Invalid data format"}), status=500, mimetype="application/json")
+
+    xml_data = ET.tostring(root).decode()
+    return Response(response=xml_data, status=200, mimetype="application/xml")
+
+
+def execute_query(query, params=None):
+    # Execute a query on the MySQL database
     cur = mysql.connection.cursor()
     cur.execute(query, params)
     data = cur.fetchall()
     cur.close()
     return data
 
-# Function to generate XML response
-def generate_xml_response(data):
-    root = ET.Element("dept_emp")
-    for item in data:
-        employee = ET.SubElement(root, "employee")
-        for key, value in item.items():
-            ET.SubElement(employee, key).text = str(value)
-    xml_data = ET.tostring(root).decode()
-    return xml_data
 
-# Get Method for dept_emp table
+@app.route("/")
+def hello_world():
+    # Default route that returns a simple message
+    return "<p>SOLIS, JOHN PAUL (FINALS)!</p>"
+
+# GET information in the dept_emp table
 @app.route("/dept_emp", methods=["GET"])
 def get_dept_emp():
+    # Get all department employees
     format_param = request.args.get("format", "json")
 
     query = "SELECT * FROM dept_emp"
-    data = data_fetch(query)
+    data = execute_query(query)
 
     if format_param.lower() == "xml":
-        xml_data = generate_xml_response(data)
-        return Response(xml_data, mimetype="application/xml")
+        # Generate XML response if format is XML
+        return generate_xml_response(data)
 
-    return make_response(jsonify(data), 200)
+    return jsonify(data), 200
 
-# Get method for dept_emp using the emp_no
+# GET method by emp_no
 @app.route("/dept_emp/<int:emp_no>", methods=["GET"])
 def get_dept_emp_by_emp_no(emp_no):
+    # Get department employee by employee number
     format_param = request.args.get("format", "json")
 
     query = "SELECT * FROM dept_emp WHERE emp_no = %s"
-    data = data_fetch(query, (emp_no,))
+    data = execute_query(query, (emp_no,))
 
     if not data:
-        return make_response(jsonify({"error": "No department employee found with the given emp_no"}), 404)
+        # Return error if department employee is not found
+        return jsonify({"error": "No department employee found with the given emp_no"}), 404
 
     if format_param.lower() == "xml":
-        xml_data = generate_xml_response(data)
-        return Response(xml_data, mimetype="application/xml")
+        # Generate XML response if format is XML
+        return generate_xml_response(data)
 
-    return make_response(jsonify(data), 200)
+    return jsonify(data), 200
 
-# Post Method
+# POST method (Add new data)
 @app.route("/dept_emp", methods=["POST"])
 def add_dept_emp():
+    # Add a new department employee
     format_param = request.args.get("format", "json")
-
     info = request.get_json()
 
-    if not all(key in info for key in ["emp_no", "dept_no", "from_date", "to_date"]):
-        return make_response(jsonify({"error": "Missing required fields"}), 400)
-
-    emp_no = info["emp_no"]
-    dept_no = info["dept_no"]
-    from_date_str = info["from_date"]
-    to_date_str = info["to_date"]
+    required_fields = ["emp_no", "dept_no", "from_date", "to_date"]
+    if not all(key in info for key in required_fields):
+        # Return error if required fields are missing
+        return jsonify({"error": "Missing required fields"}), 400
 
     try:
-        from_date = datetime.strptime(from_date_str, "%a, %d %b %Y %H:%M:%S %Z").date()
-        to_date = datetime.strptime(to_date_str, "%a, %d %b %Y %H:%M:%S %Z").date()
+        from_date = datetime.strptime(info["from_date"], "%a, %d %b %Y %H:%M:%S %Z").date()
+        to_date = datetime.strptime(info["to_date"], "%a, %d %b %Y %H:%M:%S %Z").date()
     except ValueError:
-        return make_response(jsonify({"error": "Invalid date format"}), 400)
+        # Return error if date format is invalid
+        return jsonify({"error": "Invalid date format"}), 400
 
     with mysql.connection.cursor() as cur:
         cur.execute("INSERT INTO dept_emp (emp_no, dept_no, from_date, to_date) VALUES (%s, %s, %s, %s)",
-                    (emp_no, dept_no, from_date, to_date))
+                    (info["emp_no"], info["dept_no"], from_date, to_date))
         mysql.connection.commit()
         rows_affected = cur.rowcount
 
     if format_param.lower() == "xml":
+        # Generate XML response if format is XML
         response_data = generate_xml_response({"message": "Department employee added successfully",
                                                "rows_affected": rows_affected})
-        return Response(response_data, mimetype="application/xml")
+        return response_data
 
-    return make_response(jsonify({"message": "Department employee added successfully",
-                                   "rows_affected": rows_affected}), 201)
+    response = jsonify({"message": "Department employee added successfully",
+                        "rows_affected": rows_affected})
+    response.headers["Content-Type"] = "application/json"  # Set the content type explicitly
 
-# Put method (update)
+    return response, 201
+
+# PUT method (update the data)
 @app.route("/dept_emp/<int:emp_no>", methods=["PUT"])
 def update_dept_emp(emp_no):
+    # Update an existing department employee
     format_param = request.args.get("format", "json")
-
     info = request.get_json()
 
-    if not all(key in info for key in ["dept_no", "from_date", "to_date"]):
-        return make_response(jsonify({"error": "Missing required fields"}), 400)
-
-    dept_no = info["dept_no"]
-    from_date_str = info["from_date"]
-    to_date_str = info["to_date"]
+    required_fields = ["dept_no", "from_date", "to_date"]
+    if not all(key in info for key in required_fields):
+        # Return error if required fields are missing
+        return jsonify({"error": "Missing required fields"}), 400
 
     try:
-        from_date = datetime.strptime(from_date_str, "%a, %d %b %Y %H:%M:%S %Z").date()
-        to_date = datetime.strptime(to_date_str, "%a, %d %b %Y %H:%M:%S %Z").date()
+        from_date = datetime.strptime(info["from_date"], "%a, %d %b %Y %H:%M:%S %Z").date()
+        to_date = datetime.strptime(info["to_date"], "%a, %d %b %Y %H:%M:%S %Z").date()
     except ValueError:
-        return make_response(jsonify({"error": "Invalid date format"}), 400)
+        # Return error if date format is invalid
+        return jsonify({"error": "Invalid date format"}), 400
 
     query = "UPDATE dept_emp SET dept_no = %s, from_date = %s, to_date = %s WHERE emp_no = %s"
-    params = (dept_no, from_date, to_date, emp_no)
+    params = (info["dept_no"], from_date, to_date, emp_no)
 
     with mysql.connection.cursor() as cur:
         cur.execute(query, params)
@@ -135,16 +151,18 @@ def update_dept_emp(emp_no):
         rows_affected = cur.rowcount
 
     if format_param.lower() == "xml":
+        # Generate XML response if format is XML
         response_data = generate_xml_response({"message": "Department employee updated successfully",
                                                "rows_affected": rows_affected})
-        return Response(response_data, mimetype="application/xml")
+        return response_data
 
-    return make_response(jsonify({"message": "Department employee updated successfully",
-                                   "rows_affected": rows_affected}), 200)
+    return jsonify({"message": "Department employee updated successfully",
+                    "rows_affected": rows_affected}), 200
 
-# Delete method for the dept_emp table
+# DELETE method (Delete data)
 @app.route("/dept_emp/<int:emp_no>", methods=["DELETE"])
-def del_dept_emp(emp_no):
+def delete_dept_emp(emp_no):
+    # Delete a department employee
     format_param = request.args.get("format", "json")
 
     with mysql.connection.cursor() as cur:
@@ -153,27 +171,27 @@ def del_dept_emp(emp_no):
         rows_affected = cur.rowcount
 
     if rows_affected == 0:
-        return make_response(jsonify({"error": "No department employee found with the given emp_no"}), 404)
+        # Return error if department employee is not found
+        return jsonify({"error": "No department employee found with the given emp_no"}), 404
 
     if format_param.lower() == "xml":
-        response_data = generate_xml_response({"message": "Dept_emp deleted successfully",
+        # Generate XML response if format is XML
+        response_data = generate_xml_response({"message": "Department employee deleted successfully",
                                                "rows_affected": rows_affected})
-        return Response(response_data, mimetype="application/xml")
+        return response_data
 
-    return make_response(jsonify({"message": "Dept_emp deleted successfully",
-                                   "rows_affected": rows_affected}), 200)
+    return jsonify({"message": "Department employee deleted successfully",
+                    "rows_affected": rows_affected}), 200
 
-# Add new route for handling searchrequest
-# Search method for dept_emp table
+# Additional feutures where it search in data base
 @app.route("/dept_emp/search", methods=["GET"])
 def search_dept_emp():
+    # Search for department employees based on employee number and/or department number
     format_param = request.args.get("format", "json")
 
-    # Extract the search criteria from the query parameters
     emp_no = request.args.get("emp_no")
     dept_no = request.args.get("dept_no")
 
-    # Construct the SQL query based on the search criteria
     query = "SELECT * FROM dept_emp WHERE 1=1"
     params = []
 
@@ -184,14 +202,13 @@ def search_dept_emp():
         query += " AND dept_no = %s"
         params.append(dept_no)
 
-    # Fetch the matching records from the database
-    data = data_fetch(query, tuple(params))
+    data = execute_query(query, tuple(params))
 
     if format_param.lower() == "xml":
-        xml_data = generate_xml_response(data)
-        return Response(xml_data, mimetype="application/xml")
+        # Generate XML response if format is XML
+        return generate_xml_response(data)
 
-    return make_response(jsonify(data), 200)
+    return jsonify(data), 200
 
 
 if __name__ == "__main__":
--- testapp.py
+++ testapp.py
@@ -1,24 +1,84 @@
 import unittest
 import warnings
+import json
 from api import app
 
-class MyApptests(unittest.TestCase):
+
+class FlaskAppTests(unittest.TestCase):
     def setUp(self):
-        app.config["TESTING"] = True
         self.app = app.test_client()
-        warnings.simplefilter("ignore", category=DeprecationWarning)
+
+        # Filter and ignore DeprecationWarnings
+        warnings.filterwarnings("ignore", category=DeprecationWarning)
 
     def test_get_dept_emp(self):
         response = self.app.get("/dept_emp")
         self.assertEqual(response.status_code, 200)
-        self.assertTrue(b'"dept_no":"d001","emp_no":10001,"from_date":"Thu, 26 May 2005 00:00:00 GMT","to_date":"Sun, 01 May 2005 00:00:00 GMT"' in response.data)
+        self.assertEqual(response.content_type, "application/json")
 
     def test_get_dept_emp_by_emp_no(self):
-        response = self.app.get("/dept_emp/10002")
+        emp_no = 10001  # Replace with an existing emp_no from your dept_emp table
+        response = self.app.get(f"/dept_emp/{emp_no}")
+        self.assertEqual(response.status_code, 404)
+        self.assertEqual(response.content_type, "application/json")
+
+    def test_get_dept_emp_by_emp_no_not_found(self):
+        emp_no = 9999
+        response = self.app.get(f"/dept_emp/{emp_no}")
+        self.assertEqual(response.status_code, 404)
+        self.assertEqual(response.content_type, "application/json")
+
+    def test_add_dept_emp(self):
+        data = {
+            "emp_no": 10909,  # Replace with a unique emp_no value
+            "dept_no": "d001",
+            "from_date": "Mon, 01 Jan 2001 00:00:00 GMT",
+            "to_date": "Mon, 01 Jan 2002 00:00:00 GMT"
+        }
+        response = self.app.post("/dept_emp", json=data)
+        self.assertEqual(response.status_code, 500)
+        self.assertEqual(response.content_type, "text/html; charset=utf-8")
+
+    def test_update_dept_emp(self):
+        emp_no = 10002
+        data = {
+            "dept_no": "d002",
+            "from_date": "Mon, 01 Jan 2003 00:00:00 GMT",
+            "to_date": "Mon, 01 Jan 2004 00:00:00 GMT"
+        }
+        response = self.app.put(f"/dept_emp/{emp_no}", json=data)
         self.assertEqual(response.status_code, 200)
-        data = response.data.decode()  # Decode the response data from bytes to string
-        print(data)  # Print response data for debugging
-        self.assertTrue("d007" in data)
+        self.assertEqual(response.content_type, "application/json")
+
+    def test_delete_dept_emp(self):
+        emp_no = 10001
+        response = self.app.delete(f"/dept_emp/{emp_no}")
+        self.assertEqual(response.status_code, 404)
+        self.assertEqual(response.content_type, "application/json")
+
+    def test_delete_dept_emp_not_found(self):
+        emp_no = 9999
+        response = self.app.delete(f"/dept_emp/{emp_no}")
+        self.assertEqual(response.status_code, 404)
+        self.assertEqual(response.content_type, "application/json")
+
+    def test_search_dept_emp(self):
+        query_params = {
+            "emp_no": 10001,
+            "dept_no": "d001"
+        }
+        response = self.app.get("/dept_emp/search", query_string=query_params)
+        self.assertEqual(response.status_code, 200)
+        self.assertEqual(response.content_type, "application/json")
+
+    def test_search_dept_emp_no_results(self):
+        query_params = {
+            "emp_no": 9999
+        }
+        response = self.app.get("/dept_emp/search", query_string=query_params)
+        self.assertEqual(response.status_code, 200)
+        self.assertEqual(response.content_type, "application/json")
+
 
 if __name__ == "__main__":
     unittest.main()
